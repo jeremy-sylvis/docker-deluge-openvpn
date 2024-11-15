b7bc2eb6a535e7778bf768a6b0978d5c1c7bd4f3
Jeremy Sylvis <jeremy.sylvis@gmail.com>
2024-11-14 20:20:15 -0600

    Remove a diagnostic comment

--- root/etc/openvpn/tunnelUp.sh
+++ root/etc/openvpn/tunnelUp.sh
@@ -137,7 +137,6 @@ for i in sys.stdin.readlines():
   fi
 
   # Update Deluge config with this new port
-  # sed: -e expression #1, char 53: unterminated `s' command
   echo "Updating Deluge config to listen on forwarded UDP port '$NATPMPC_FORWARDED_PORT'..."
   sed -i -E "s/.*listen_ports.*/    \"listen_ports\": \[ $NATPMPC_FORWARDED_PORT \],\n/" "/etc/config/core.conf"
   
