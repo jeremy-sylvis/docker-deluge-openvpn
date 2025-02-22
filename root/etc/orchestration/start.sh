3bdf8612869300510c5fe70006ae46e822def684
Jeremy Sylvis <jeremy.sylvis@gmail.com>
2025-02-22 13:37:14 -0600

    Simplify orchestration/start.sh tmp file cleanup

--- root/etc/orchestration/start.sh
+++ root/etc/orchestration/start.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
 # Cleanup temp files
-tempFiles=@(
-    "/tmp/gateway_initialized"
-    "/tmp/openvpn_exited"
-    "/tmp/natpmpc_query_success"
-    "/tmp/natpmpc_forwarded_port"
-)
-for $path in ${tempFiles[@]}; do
-    if [ -f "$path" ]; then
-        rm "$path"
-    fi
-done
+find /tmp -exec rm -rf {} \;
 
 # Begin OpenVPN initialization
 ./etc/openvpn/init.sh
